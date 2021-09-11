8949bf2e44c40476b835525b6f3d85128ba65bc6
ifrabatool123 <40837809+ifrabatool123@users.noreply.github.com>
2021-09-11 13:02:13 -0700

    PeriodicTable

---
+++ main.js
@@ -0,0 +1,12 @@
+/*
+ * Auto-generated content from the Brackets New Project extension.  Enjoy!
+ */
+var creditCard={
+    name:"John",
+    
+};
+Object.defineProperty(creditCard,"name",
+                     {
+    enumerable: false
+});
+console.log("name" in creditCard);
